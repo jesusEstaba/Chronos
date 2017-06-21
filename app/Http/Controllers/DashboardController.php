0774b89a24481091d9eab0b707a54589c32a8322
Jesus Estaba <jeec.estaba@gmail.com>
2017-06-21 06:45:36 -0400

    Datos sencillos para el dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,27 @@
 namespace Cronos\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Cronos\Partitie;
+use Cronos\Material;
+use Cronos\Equipment;
+use Cronos\Workforce;
+use Cronos\Project;
+use Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-    	return view('dashboard');
+    	$numOfMaterials = Material::where('companieId', Auth::user()->companieId)->count();
+    	$numOfEquipments = Equipment::where('companieId', Auth::user()->companieId)->count();
+    	$numOfWorkforces = Workforce::where('companieId', Auth::user()->companieId)->count();
+    	$numOfPartities = Partitie::where('companieId', Auth::user()->companieId)->count();
+
+    	return view('dashboard', compact(
+    		'numOfMaterials',
+			'numOfEquipments',
+			'numOfWorkforces',
+			'numOfPartities'
+    	));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,5 +2,43 @@
 @section('activeId', 'dashboard')
 
 @section('content')
-	<h2>Dashboard Master Race</h2>
+	<h2>Dashboard</h2>
+	<div class="box">
+		<div class="box-body">
+			<div class="rowx">
+				<div class="col-xs-3x">
+					<div class="big-badge-num">
+						<span class="badge-number">
+							{{$numOfPartities}}
+						</span>
+						<span class="badge-title">Partidas</span>
+					</div>
+				</div>
+				<div class="col-xs-3">
+					<div class="big-badge-num">
+						<span class="badge-number">
+							{{$numOfMaterials}}
+						</span>
+						<span class="badge-title">Materiales</span>
+					</div>
+				</div>
+				<div class="col-xs-3">
+					<div class="big-badge-num">
+						<span class="badge-number">
+							{{$numOfEquipments}}
+						</span>
+						<span class="badge-title">Equipos</span>
+					</div>
+				</div>
+				<div class="col-xs-3">
+					<div class="big-badge-num">
+						<span class="badge-number">
+							{{$numOfWorkforces}}
+						</span>
+						<span class="badge-title">Mano de Obra</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 @stop
