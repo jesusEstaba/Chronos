64aecabcd9d3ac7cc032cd90ca12d0c0d609683f
Jesus Estaba <jeec.estaba@gmail.com>
2017-07-19 12:46:04 -0400

    fix texto

--- resources/views/partitie/create.blade.php
+++ resources/views/partitie/create.blade.php
@@ -534,7 +534,7 @@ function totalInworkforces() {
 							<div class="modal-dialog" role="document">
 								<div class="modal-content">
 									<div class="modal-header">
-										<h5 class="modal-title" id="exampleModalLabel">Msno de Obra</h5>
+										<h5 class="modal-title" id="exampleModalLabel">Mano de Obra</h5>
 										<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 										<span aria-hidden="true">&times;</span>
 										</button>
